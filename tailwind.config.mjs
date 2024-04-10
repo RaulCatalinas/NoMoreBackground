400565e7f903e55b20a3a07bc54944a4aa61986a
Raul Catalinas <105791463+RaulCatalinas@users.noreply.github.com>
2024-04-10 23:28:49 +0200

    chore(Root): Updated 'tailwind.config.mjs' file

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{astro,tsx}'],
+  content: ['./src/**/*.astro'],
   theme: {
     extend: {
       fontFamily: {
